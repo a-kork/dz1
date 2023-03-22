c6f44d6b8c8e42ffc4d36506d2c1ad8a7dcbef0c
Alim Korkmazov <alimkork9@gmail.com>
2023-03-22 14:57:37 +0300

    add help option

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,11 @@
 
 
 
-int main () {
+int main (int argc, char* argv[]) {
+    if (argc >1 && std::string_view{argv[1]}== "--help"){
+        std ::cout << "Show help" << std::endl;
+        return 0;
+    }
     print_hello ();
     std::cout << "end" << std::endl;
     return 0;
